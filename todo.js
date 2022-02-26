647f23379c1ea908cf8664ee16cd82c28e97fcd0
allendo1998 <allendo1998@gmail.com>
2022-02-26 17:03:10 +1100

    added input value to checklist

--- todo.js
+++ todo.js
@@ -1,10 +1,30 @@
 const todo = () => {
     var activity = document.getElementById("activity");
-    var button = document.getElementById("add");
+    var debug = document.getElementById("debug");
+    var list = document.getElementById("list");
+    const todoList = [];
 
     activity.addEventListener("keyup", function(event) {
         if(event.key === 'Enter'){
-            console.log("enter has been entered");
+            var user_input = activity.value;
+            if(user_input.length > 0) {
+                console.log(user_input);
+                console.log(user_input.length);
+                activity.value = null;
+                todoList.push(user_input);
+                display_task(user_input);
+            }
+        }
+    })
+
+    function display_task(user_input) {
+        console.log("trying to change");
+        list.innerHTML +="<li><input type='checkbox'>" + user_input + "</li>";
+    }
+
+    debug.addEventListener('click', () =>{
+        for(var i = 0; i < todoList.length; i++){
+            console.log(todoList[i]);
         }
     })
 }
